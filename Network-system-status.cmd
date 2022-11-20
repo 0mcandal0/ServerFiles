b80d5b0c840f0914085789b33839b4d6e04be2b4
Chatrughan Prasad <chatrughan.prasad@outlook.com>
2022-11-20 11:00:17 +0530

    Create Network-system-status.cmd

---
+++ Network-system-status.cmd
@@ -0,0 +1,14 @@
+systeminfo > C:\Windows\Temp\se_support_53D7014D\SystemInfo.txt
+ipconfig > C:\Windows\Temp\se_support_53D7014D\ipconfig.txt
+netsh dump > C:\Windows\Temp\se_support_53D7014D\netsh.txt
+route print > C:\Windows\Temp\se_support_53D7014D\route.txt
+netstat -nab > C:\Windows\Temp\se_support_53D7014D\netstat_nab.txt
+netstat -nao > C:\Windows\Temp\se_support_53D7014D\netstat_nao.txt
+netstat -na > C:\Windows\Temp\se_support_53D7014D\netstat_na.txt
+netstat -fab > C:\Windows\Temp\se_support_53D7014D\netstat_fab.txt
+netstat -fao > C:\Windows\Temp\se_support_53D7014D\netstat_fao.txt
+netstat -fa > C:\Windows\Temp\se_support_53D7014D\netstat_fa.txt
+netstat -ab > C:\Windows\Temp\se_support_53D7014D\netstat_ab.txt
+netstat -ao > C:\Windows\Temp\se_support_53D7014D\netstat_ao.txt
+netstat -a > C:\Windows\Temp\se_support_53D7014D\netstat_a.txt
+"C:\Program Files\Common Files\Microsoft Shared\MSInfo\msinfo32.exe" /report C:\Windows\Temp\se_support_53D7014D\SystemInfo.txt
